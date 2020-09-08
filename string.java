e9b5decb2889ed746b43d690ff232959fd9daad1
fadlyalbar <fadlyalbar9f@gmail.com>
2020-09-08 18:38:48 +0700

    uts

---
+++ string.java
@@ -0,0 +1,38 @@
+package uts;
+
+// nama : fadly albar
+// kelas : 12 rpl b
+
+public class string {
+
+	public static void main(String[] args) {
+                 //0123456789012345678901234567				
+	String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+	String letter = Character.toString(text.charAt(5));
+	String letter2 = Character.toString(text.charAt(0));
+	String letter3 = Character.toString(text.charAt(3));
+	String letter4 = Character.toString(text.charAt(11));
+	String letter5 = Character.toString(text.charAt(24));
+	/////////////////////////////////////////////////////
+	String letter6 = Character.toString(text.charAt(27));
+	/////////////////////////////////////////////////////
+	String letter7 = Character.toString(text.charAt(0));
+	String letter8 = Character.toString(text.charAt(11));
+	String letter9 = Character.toString(text.charAt(1));
+	String letter10 = Character.toString(text.charAt(0));
+	String letter11 = Character.toString(text.charAt(17));
+	System.out.print(letter);
+	System.out.print(letter2);
+	System.out.print(letter3);
+	System.out.print(letter4);
+	System.out.print(letter5);
+	System.out.print(letter6);
+	System.out.print(letter7);
+	System.out.print(letter8);
+	System.out.print(letter9);
+	System.out.print(letter10);
+	System.out.print(letter11);
+	
+	}
+
+}
